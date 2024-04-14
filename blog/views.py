4e20e9aa4079eaec8f991910eed226818cdac712
Motoshi Suzuki <101000230+motoshi-suzuki-hp@users.noreply.github.com>
2024-04-14 21:46:39 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm  # 後で作成
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
